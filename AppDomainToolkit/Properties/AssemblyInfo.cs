85fd5a3768bc5ad83d3438b4847316cd5d6aeb36
Jeremy Duvall <matchless.tone@gmail.com>
2013-06-22 13:49:48 -0400

    Merged contributor code

--- AppDomainToolkit/Properties/AssemblyInfo.cs
+++ AppDomainToolkit/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("8220c5ce-9cb4-485f-ae78-f2a9f2cbd754")]
 
 // Versioning
-[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.4.1")]
 
 // Unit tests.
 [assembly: InternalsVisibleTo("AppDomainToolkit.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a5909562fdf63a75fcd4a17dc013ab1d90a755323807a188b8f1703206efc99ed5a06d2e19418ade665c90b52d210dae8e06aac1ba67a51bafc4b34b4c837ad38261e801263fab8d0c0f3c75b2ca1e92af4dafe21b4e272e0824dfa464fcc690b4a305e4f02ff6805c000e81f49857cf2decbea73268b6917a31508017ca93a3")]
